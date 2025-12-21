d5e035da67060a3d5587fe5ad2a2970e26a9a290
Alluri Vishal <154000801+AVishal9@users.noreply.github.com>
2025-12-21 22:15:25 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# @erp.feature 4me4nledo8
-
-def calculate_discount(price, rate):
-    """
-    Calculates the final price after discount.
-    """
-    if rate > 1.0:
-        return price
-    return price * (1 - rate)
